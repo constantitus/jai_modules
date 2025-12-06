774a03b4b5d3a709812de1faa8ba691e142b15d9
constantitus <constantitus@gmail.com>
2025-12-06 18:46:30 +0200

    updated angle, glfw and added other small metaprogram plugins

---
+++ glfw/source/glfw_native.h
@@ -0,0 +1,26 @@
+#if defined(_WIN32)
+# define _GLFW_WIN32
+# define GLFW_EXPOSE_NATIVE_WIN32
+# define GLFW_EXPOSE_NATIVE_WGL
+#endif
+
+#if defined(__linux__)
+# if !defined(NO_WAYLAND)
+#  define _GLFW_WAYLAND
+#  define GLFW_EXPOSE_NATIVE_WAYLAND
+# endif
+# define _GLFW_X11
+# define GLFW_EXPOSE_NATIVE_X11
+# define GLFW_EXPOSE_NATIVE_GLX
+#endif
+
+#if defined(__APPLE__)
+# define _GLFW_COCOA
+# define _GLFW_USE_MENUBAR
+# define _GLFW_USE_RETINA
+# define GLFW_EXPOSE_NATIVE_COCOA
+# define GLFW_EXPOSE_NATIVE_NSGL
+#endif
+
+#include "glfw/include/GLFW/glfw3.h"
+#include "glfw/include/GLFW/glfw3native.h"
